5352a8be458d5cf32d9423cc00d3733147b5f5ef
Sergey-Markov <1nativefl@gmail.com>
2021-06-20 23:50:26 +0300

    task complete

--- index.js
+++ index.js
@@ -1,51 +1,110 @@
 
+
+'use strict';
+
 const refs = {
-    daysEl: document.querySelector('[data-value=days]'),
-    hoursEl: document.querySelector('[data-value=hours]'),
-    minsEl: document.querySelector('[data-value=mins]'),
-    secsEl: document.querySelector('[data-value=secs]'),
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    clearBtn: document.querySelector('[data-action="clear"]'),
 };
 
-const timer = {
-    start(){
-        const expectedDate = new Date(2022, 1, 1);
-        setInterval(() => {
-            
-            calculateDate(expectedDate)
-            
-            
+class CountdownTimer {
+    constructor({targetDate, selector}){
+        this.intervalId = null;
+        this.isActive = false;
+        this.targetDate = targetDate;
+        this.selector = selector;
+    }
+
+    start() {
+        if (this.isActive) {
+            return;
+        };
+        
+        this.isActive = true;
+        
+        document.querySelector(this.selector)
+            .innerHTML = this.createTimerTemplateEls({ days: this.pad(0), hours: this.pad(0), mins: this.pad(0), secs: this.pad(0) });
+        
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            const timeDifference = this.targetDate - currentTime;
+            const leftTime = this.getTimeElements(timeDifference);
+            this.unpdateTimerElements(leftTime);
         }, 1000);
-    },
-};
+    };
 
-function calculateDate (expectedDate) {
-    const currentTime = Date.now();
-    const timeLeft = expectedDate - currentTime;
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = pad(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((timeLeft % (1000 * 60)) / 1000)); 
+
+    stop(){
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        // const leftTime = this.getTimeElements(0);
+        // this.unpdateTimerElements(leftTime);
+    };
+
+    clear() {
+        document.querySelector(this.selector)
+            .innerHTML = '';
+        clearInterval(this.intervalId);
+        this.isActive = false;
+    }
+
+    getTimeElements (time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     
-    console.log(timeLeft);
+        return { days, hours, mins, secs };
+    };
 
-    refs.daysEl.textContent = days;
-    refs.hoursEl.textContent = hours;
-    refs.minsEl.textContent = mins;
-    refs.secsEl.textContent = secs;
+    pad(value) {
+        return String(value).padStart(2, '0');
+    };
+
+    createTimerTemplateEls ({ days, hours, mins, secs }) {
+        return `<div class="field">
+            <span class="value" data-value="days">${days}</span>
+            <span class="label">Days</span>
+            </div>
+    
+             <div class="field">
+              <span class="value" data-value="hours">${hours}</span>
+              <span class="label">Hours</span>
+            </div>
+          
+            <div class="field">
+              <span class="value" data-value="mins">${mins}</span>
+              <span class="label">Minutes</span>
+            </div>
+          
+            <div class="field">
+              <span class="value" data-value="secs">${secs}</span>
+              <span class="label">Seconds</span>
+            </div>`;
+    };
+
+    unpdateTimerElements ({ days, hours, mins, secs }) {
+        document.querySelector('[data-value=days]').textContent = `${days}`;
+        document.querySelector('[data-value=hours]').textContent = `${hours}`; 
+        document.querySelector('[data-value=mins]').textContent = `${mins}`;
+        document.querySelector('[data-value=secs]').textContent = `${secs}`;
+    };
 };
 
-function pad (value){
-    return String(value).padStart(2, 0);
-} 
-timer.start();
-// function CountdownTimer({selector, targetDate}){
-//     this.selector = selector;
-//     this.targetDate = targetDate;
+const timer = new CountdownTimer({
+    targetDate: new Date('March 21 2022'),
+    selector: '#timer-1',
+});
 
-//  
-// };
+refs.startBtn.addEventListener('click', () => {
+    timer.start();
+});
 
-// const firstTimer = new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2022'),
-//   });
+refs.stopBtn.addEventListener('click', () => {
+    timer.stop();
+});
+
+refs.clearBtn.addEventListener('click', () => {
+    timer.clear();
+});
